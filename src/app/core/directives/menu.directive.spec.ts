51c12863c319410c14e489b9e698e5a71026f32f
hushcha <375291601334@mail.ru>
2019-10-26 18:26:27 +0300

    fix test for MenuDirective

--- src/app/core/directives/menu.directive.spec.ts
+++ src/app/core/directives/menu.directive.spec.ts
@@ -1,12 +1,27 @@
-import { ViewContainerRef } from "@angular/core";
+import { async, TestBed } from '@angular/core/testing';
 
 import { MenuDirective } from './menu.directive';
+import { ViewContainerRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'view-container-ref-demo',
+  template: `<ng-template appMenu></ng-template>`,
+})
+class ParentComponent {
+  constructor(public viewContainerRef: ViewContainerRef) {}
+}
 
 describe('MenuDirective', () => {
-  let viewContainerRef: ViewContainerRef;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ParentComponent],
+    })
+    .compileComponents();
+  }));
 
   it('should create an instance', () => {
-    const directive = new MenuDirective(viewContainerRef);
+    const fixture = TestBed.createComponent(ParentComponent);
+    const directive = new MenuDirective(fixture.componentInstance.viewContainerRef);
     expect(directive).toBeTruthy();
   });
 });
