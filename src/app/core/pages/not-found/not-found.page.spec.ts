af246a39e8c2e6bcacf5c52e8b3fd63d0ba7c543
Anastasiya Hushcha <Anastasiya_Hushcha@epam.com>
2019-12-05 11:14:57 +0300

    improve design of not found page

--- src/app/core/pages/not-found/not-found.page.spec.ts
+++ src/app/core/pages/not-found/not-found.page.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 import { NotFoundPageComponent } from './not-found.page';
+
+class MockRouter {
+  navigate() {}
+}
 
 describe('NotFoundPageComponent', () => {
   let component: NotFoundPageComponent;
   let fixture: ComponentFixture<NotFoundPageComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NotFoundPageComponent],
+      providers: [
+        { provide: Router, useClass: MockRouter },
+      ],
     })
     .compileComponents();
   }));
@@ -16,10 +26,18 @@ describe('NotFoundPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NotFoundPageComponent);
     component = fixture.componentInstance;
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to courses page once clicking Home button', () => {
+    spyOn(router, 'navigate');
+
+    fixture.debugElement.query(By.css('button')).triggerEventHandler('click', null);
+    expect(router.navigate).toHaveBeenCalledWith(['']);
   });
 });
